b324ebaa93b4ddba834674c380bdcd3527a513a9
Vela <davidbak38@gmail.com>
2023-05-07 18:00:24 +0200

    ajout de l'importance

--- assets/php/Tasks/listTasks.php
+++ assets/php/Tasks/listTasks.php
@@ -4,8 +4,12 @@
             $removeUrl = 'delete=' . $task->getId();
             $removeLink = '<a href="./pageTasks.php?' . $removeUrl . '"><img class="button-delete" src="../../img/delete_button-removebg-preview.png" alt="bouton delete"></a>';
             $checkbox = '<input type="checkbox" class="check_progress" name="task-' . $task->getId() . '">';
-
-            echo ('<li>'.$removeLink.$task->getTitle() . $checkbox . '</li>');
+            if ($task->getImportant() == 1){
+                echo ('<li>'.$removeLink.$task->getTitle() . " (important)" . $checkbox . '</li>');
+            } else {
+                echo ('<li>'.$removeLink.$task->getTitle() . $checkbox . '</li>');
+            }
+            
         }
     ?>
 </ul>
