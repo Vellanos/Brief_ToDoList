032ac0da06fc76b15690b45c3ea41f9a0c634b46
Vela <davidbak38@gmail.com>
2023-05-10 08:34:58 +0200

    ajout du style rouge pour tache importante

--- assets/php/Tasks/listTasks.php
+++ assets/php/Tasks/listTasks.php
@@ -5,7 +5,8 @@
             $removeLink = '<a href="./pageTasks.php?' . $removeUrl . '"><img class="button-delete" src="../../img/delete_button.png" alt="bouton delete"></a>';
             $checkbox = '<input type="checkbox" class="check_progress" name="task-' . $task->getId() . '">';
             if ($task->getImportant() == 1){
-                echo ('<li>'.$removeLink.$task->getTitle() . " (important)" . $checkbox . '</li>');
+                echo ('<li class="important">'.$removeLink.$task->getTitle() . " (important)" . $checkbox . '</li>');
+
             } else {
                 echo ('<li>'.$removeLink.$task->getTitle() . $checkbox . '</li>');
             }
